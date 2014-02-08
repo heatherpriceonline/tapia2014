6bd180d875e3b4c94d479b95d7712662f262df6f
hprice11 <heather.price91@gmail.com>
2014-02-08 14:38:21 -0500

    Changed name of In The Future tab.

--- Now/gen/com/example/now/R.java
+++ Now/gen/com/example/now/R.java
@@ -24,7 +24,7 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int In the Future=0x7f080006;
+        public static final int Later=0x7f080006;
         public static final int Today=0x7f080000;
         public static final int Tomorrow=0x7f080005;
         public static final int action_settings=0x7f080007;
