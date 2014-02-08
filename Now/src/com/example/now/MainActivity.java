89ce23ffee31715bd325a0adcc0e4ffc7a6146e6
Eleane Jao <eleanew83@berkeley.edu>
2014-02-08 14:50:22 -0800

    Finished app for tapia 2014 (unfinished).

--- Now/src/com/example/now/MainActivity.java
+++ Now/src/com/example/now/MainActivity.java
@@ -1,22 +1,48 @@
 package com.example.now;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity{
 
+	Button todaybutton;
+	EditText todoTextEditText;
+	TextView todoTextView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		todaybutton = (Button) findViewById(R.id.todayButton);
+		
+		todoTextEditText = (EditText) findViewById(R.id.todoText);
+		
+		todoTextView = (TextView) findViewById(R.id.textView);
+			todaybutton.setOnClickListener (new View.OnClickListener() {
+			    public void onClick(View v) {
+			    	String stodoText = todoTextEditText.getText().toString(); //string
+			    	//Log.d("TAG", stodoText);
+			    	if (!stodoText.matches("") && todoTextView.getText().equals("")) {
+			    		todoTextView.setText(stodoText);
+			    	} else {
+			    		todoTextView.setText(todoTextView.getText() + "\n" + stodoText);	
+			    	}
+			    	todoTextEditText.setText("");
+			    }
+			});
 	}
-
+	/*
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
+	*/
 
 }
